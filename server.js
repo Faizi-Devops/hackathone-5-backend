b16e067e2b0d0253ad1a35cb8c6ea2131b11eb15
Faizan Ali <151016446+faizan1236@users.noreply.github.com>
2023-12-28 00:05:26 -0800

    removed my-app

--- server.js
+++ server.js
@@ -3,9 +3,8 @@ const connectDB = require("./db/connectdb");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const userRegister = require("./routes/User");
-const category = require("./routes/Category")
-const mongoose = require("mongoose")
-
+const category = require("./routes/Category");
+const mongoose = require("mongoose");
 
 const todo = require("./routes/Todo");
 dotenv.config();
@@ -13,24 +12,25 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/user",userRegister)
+app.use("/user", userRegister);
 app.use("/todo", todo);
-app.use("/cat",category)
+app.use("/cat", category);
 const port = 5000;
-
-// Make sure to define or retrieve your DATABASE_URL properly
-const DATABASE_URL = process.env.DATABASE_URL;
-// Database
-mongoose.connect(DATABASE_URL)
-    .then(()=>{
-        console.log('Database connection successful')
-    }).catch((err)=>{
-        console.log(err)
-    })
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+// Make sure to define or retrieve your DATABASE_URL properly
+const DATABASE_URL = process.env.DATABASE_URL;
+// Database
+mongoose
+  .connect(DATABASE_URL)
+  .then(() => {
+    console.log("Database connection successful");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
